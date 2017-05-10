bf27c64c8be25c1ed3327c9275499f1b6e308be5
devneko <dotneet@gmail.com>
2017-05-10 12:22:47 +0900

    Add example

---
+++ examples/basic.js
@@ -0,0 +1,11 @@
+const Chromy = require('../src')
+
+let chromy = new Chromy()
+chromy.chain()
+      .goto('http://example.com/')
+      .evaluate(() => {
+        return document.querySelectorAll('*').length
+      })
+      .result((r) => console.log(r))
+      .end()
+      .then(_ => chromy.close())
